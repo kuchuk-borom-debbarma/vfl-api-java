d73aa05fd37c4ee88ed86c9599e7b44ea405c89f
kuchuk borom debbarma <kuchukboromd@outlook.com>
2025-08-23 22:57:26 +0530

    Added mandatory maven dependencies

--- src/main/java/dev/kuku/vfl/Main.java
+++ src/main/java/dev/kuku/vfl/Main.java
@@ -1,9 +1,13 @@
 package dev.kuku.vfl;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Main {
     public static void main(String[] args) {
+        int a = 12;
+        System.out.println("a = $a");
+        log.info("a = $a");
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
