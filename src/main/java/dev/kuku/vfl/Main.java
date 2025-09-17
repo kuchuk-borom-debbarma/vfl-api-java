ddd50d0a5a86ae942591b2eb160fbfd54a6d2cb4
kuchuk borom debbarma <kuchukboromd@outlook.com>
2025-09-17 12:00:56 +0530

    Added todo

--- src/main/java/dev/kuku/vfl/Main.java
+++ src/main/java/dev/kuku/vfl/Main.java
@@ -2,9 +2,7 @@ package dev.kuku.vfl;
 
 public class Main {
     public static void main(String[] args) {
-        //TODO SLF4J Implementation
-        //Do not over complicate annotaion, static initializer class that stores data for annotation approach too
-        //advices uses them directly, rest of the function needs to have the required data passed through parameter this will keep things modular
-        //TODO buffer and flush needs to be totally safety checked
+        //TODO a way to disable certain actions easily, such as NoOp buffer, flushHandler or skipping if byteBuddy is not initialized
+        //TODO null pointer safety checks that should not break the code
     }
 }
